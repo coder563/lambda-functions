2e5469e7e04e9fa5aa5cd5f1245823a3aa3ccd0b
coder563 <walkerml563@gmail.com>
2026-02-18 23:19:28 +0000

    added ai-agent from SAM

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,8 +2,9 @@ import requests
 def handler(event, context):
    
     response = requests.get("https://httpbin.org/get")
+    print("Response from httpbin.org:", response.text)
     return {
         'statusCode': 200,
-        'body': 'Hello, This is my Lambda function!'
+        'body': 'Hello, This is my Lambda function!',
         'response_body': response.text
     }
